aec245eb7ecbd2e8a8a287895db675768ac33af5
tainguyendev <tientai101203@gmail.com>
2024-11-01 21:00:03 +0700

    làm lại dự án mới

--- src/components/learn/Mycomponent.jsx
+++ src/components/learn/Mycomponent.jsx
@@ -1,7 +1,9 @@
 import './style.css'
 const Thietlapcomponent = () => {
+    const tainguyendev = 26;
     return (
         <>
+            <div>{tainguyendev}</div>
             <div className="mau">xin chào tôi đang thực hành component = html + js</div>
             <div className="chinhsua">chào em nha</div>
         </>
